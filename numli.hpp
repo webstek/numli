383d12bf80e57fe17b2f51d1859cb3973a941643
Kyle <kyle.jr.webster@gmail.com>
2024-10-17 01:40:36 -0700

    vec4 and type aliases

--- numli.hpp
+++ numli.hpp
@@ -21,6 +21,27 @@ namespace cg {
 		vec3() : e{0,0,0} {};
 		vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
 	};
+
+	/**
+	 * 3d point type alias of vec3
+	 */
+	using point3 = vec3;
+
+
+	/**
+	 * 4 component vvector of doubles
+	 */
+	class vec4 {
+	public:
+		double e[4];
+		vec4() : e{0,0,0,1} {};
+		vec4(double e0, double e1, double e2, double e3) : e{e0, e1, e2, e3} {};
+	};
+
+	/**
+	 * rgb alpha color type alias of vec4
+	 */
+	using rgba = vec4;
 }
 
 
